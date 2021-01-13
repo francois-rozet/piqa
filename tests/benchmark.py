dca18ed62544595e2102377ca2fb01e91a8397b2
François Rozet <francois.rozet@outlook.com>
2021-01-13 12:10:36 +0100

    ⚗ Improve benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -10,6 +10,7 @@ Packages:
     pytorch-msssim: https://pypi.org/project/pytorch-msssim/
 """
 
+import contextlib
 import numpy as np
 import os
 import pandas as pd
@@ -40,7 +41,82 @@ from piqa import (
 )
 
 
-def timeit(f, n: int = 420) -> (float, float):
+LENNA = 'https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png'
+
+METRICS = {
+    'TV': lambda: (1, {
+        'piq.tv': piq.total_variation,
+        'tv': tv.tv,
+        'piq.TV': piq.TVLoss(),
+        'TV': tv.TV(),
+    }),
+    'PSNR': lambda: (2, {
+        'sk.psnr': sk.peak_signal_noise_ratio,
+        'piq.psnr': piq.psnr,
+        'psnr': psnr.psnr,
+        'kornia.PSNR': kornia.PSNRLoss(max_val=1.),
+        'PSNR': psnr.PSNR(),
+    }),
+    'SSIM': lambda: (2, {
+        'sk.ssim': lambda x, y: sk.structural_similarity(
+            x, y,
+            win_size=11,
+            multichannel=True,
+            gaussian_weights=True,
+        ),
+        'piq.ssim': piq.ssim,
+        'ssim': ssim.ssim,
+        'kornia.SSIM-halfloss': kornia.SSIM(
+            window_size=11,
+            reduction='mean',
+        ),
+        'piq.SSIM-loss': piq.SSIMLoss(),
+        'IQA.SSIM-loss': IQA.SSIM(),
+        'vainf.SSIM': vainf.SSIM(data_range=1.),
+        'SSIM': ssim.SSIM(),
+    }),
+    'MS-SSIM': lambda: (2, {
+        'piq.msssim': piq.multi_scale_ssim,
+        'msssim': ssim.msssim,
+        'piq.MSSSIM-loss': piq.MultiScaleSSIMLoss(),
+        'IQA.MSSSIM-loss': IQA.MS_SSIM(),
+        'vainf.MSSSIM': vainf.MS_SSIM(data_range=1.),
+        'MSSSIM': ssim.MSSSIM(),
+    }),
+    'LPIPS': lambda: (2, {
+        'piq.LPIPS': piq.LPIPS(),
+        'IQA.LPIPS': IQA.LPIPSvgg(),
+        'LPIPS': lpips.LPIPS(network='vgg')
+    }),
+    'GMSD': lambda: (2, {
+        'piq.gmsd': piq.gmsd,
+        'gmsd': gmsd.gmsd,
+        'piq.GMSD': piq.GMSDLoss(),
+        'IQA.GMSD': IQA.GMSD(),
+        'GMSD': gmsd.GMSD(),
+    }),
+    'MS-GMSD': lambda: (2, {
+        'piq.msgmsd': piq.multi_scale_gmsd,
+        'msgmsd': gmsd.msgmsd,
+        'piq.MSGMSD': piq.MultiScaleGMSDLoss(),
+        'MSGMSD': gmsd.MSGMSD(),
+    }),
+    'MDSI': lambda: (2, {
+        'piq.mdsi': piq.mdsi,
+        'mdsi': mdsi.mdsi,
+        'piq.MDSI-loss': piq.MDSILoss(),
+        'MDSI': mdsi.MDSI(),
+    }),
+    'HaarPSI': lambda: (2, {
+        'piq.haarpsi': piq.haarpsi,
+        'haarpsi': haarpsi.haarpsi,
+        'piq.HaarPSI-loss': piq.HaarPSILoss(),
+        'HaarPSI': haarpsi.HaarPSI(),
+    }),
+}
+
+
+def timeit(f, n: int) -> (float, float):
     cuda_start = torch.cuda.Event(enable_timing=True)
     cuda_end = torch.cuda.Event(enable_timing=True)
 
@@ -58,93 +134,45 @@ def timeit(f, n: int = 420) -> (float, float):
     return cuda_start.elapsed_time(cuda_end) / 1000, end - start
 
 
-if __name__ == '__main__':
-    lenna = 'https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png'
+def main(
+    url: str,
+    metrics: list = [],
+    batch: int = 1,
+    warm: int = 69,
+    loops: int = 420,
+    device: str = 'cuda',
+    grad: bool = True,
+    backend: bool = True,
+):
+    # Backend
+    torch.backends.cudnn.enabled = backend
+    torch.backends.cudnn.benchmark = backend
 
-    truth = Image.open(request.urlopen(lenna))
+    # Images
+    truth = Image.open(request.urlopen(url))
     noisy = truth.filter(ImageFilter.BLUR)
 
     noisy, truth = np.array(noisy), np.array(truth)
 
-    x = transforms.ToTensor()(noisy).unsqueeze(0).cuda()
-    y = transforms.ToTensor()(truth).unsqueeze(0).cuda()
+    totensor = transforms.ToTensor()
+
+    x = totensor(noisy).repeat(batch, 1, 1, 1).to(device)
+    y = totensor(truth).repeat(batch, 1, 1, 1).to(device)
 
     x.requires_grad_()
     y.requires_grad_()
 
-    metrics = {
-        ('TV', 1): {
-            'piq.tv': piq.total_variation,
-            'tv': tv.tv,
-            'piq.TV': piq.TVLoss(),
-            'TV': tv.TV(),
-        },
-        ('PSNR', 2): {
-            'sk.psnr': sk.peak_signal_noise_ratio,
-            'piq.psnr': piq.psnr,
-            'psnr': psnr.psnr,
-            'kornia.PSNR': kornia.PSNRLoss(max_val=1.),
-            'PSNR': psnr.PSNR(),
-        },
-        ('SSIM', 2): {
-            'sk.ssim': lambda x, y: sk.structural_similarity(
-                x, y,
-                win_size=11,
-                multichannel=True,
-                gaussian_weights=True,
-            ),
-            'piq.ssim': piq.ssim,
-            'ssim': ssim.ssim,
-            'kornia.SSIM-halfloss': kornia.SSIM(
-                window_size=11,
-                reduction='mean',
-            ),
-            'piq.SSIM-loss': piq.SSIMLoss(),
-            'IQA.SSIM-loss': IQA.SSIM(),
-            'vainf.SSIM': vainf.SSIM(data_range=1.),
-            'SSIM': ssim.SSIM(),
-        },
-        ('MS-SSIM', 2): {
-            'piq.msssim': piq.multi_scale_ssim,
-            'msssim': ssim.msssim,
-            'piq.MSSSIM-loss': piq.MultiScaleSSIMLoss(),
-            'IQA.MSSSIM-loss': IQA.MS_SSIM(),
-            'vainf.MSSSIM': vainf.MS_SSIM(data_range=1.),
-            'MSSSIM': ssim.MSSSIM(),
-        },
-        # ('LPIPS', 2): {
-        #     'piq.LPIPS': piq.LPIPS(),
-        #     'IQA.LPIPS': IQA.LPIPSvgg(),
-        #     'LPIPS': lpips.LPIPS(network='vgg')
-        # },
-        ('GMSD', 2): {
-            'piq.gmsd': piq.gmsd,
-            'gmsd': gmsd.gmsd,
-            'piq.GMSD': piq.GMSDLoss(),
-            'IQA.GMSD': IQA.GMSD(),
-            'GMSD': gmsd.GMSD(),
-        },
-        ('MS-GMSD', 2): {
-            'piq.msgmsd': piq.multi_scale_gmsd,
-            'msgmsd': gmsd.msgmsd,
-            'piq.MSGMSD': piq.MultiScaleGMSDLoss(),
-            'MSGMSD': gmsd.MSGMSD(),
-        },
-        ('MDSI', 2): {
-            'piq.mdsi': piq.mdsi,
-            'mdsi': mdsi.mdsi,
-            'piq.MDSI-loss': piq.MDSILoss(),
-            'MDSI': mdsi.MDSI(),
-        },
-        ('HaarPSI', 2): {
-            'piq.haarpsi': piq.haarpsi,
-            'haarpsi': haarpsi.haarpsi,
-            'piq.HaarPSI-loss': piq.HaarPSILoss(),
-            'HaarPSI': haarpsi.HaarPSI(),
-        },
-    }
+    # Metrics
+    if metrics:
+        metrics = {
+            k: v() for (k, v) in METRICS.items()
+            if k in metrics
+        }
+    else:
+        metrics = {k: v() for (k, v) in METRICS.items()}
 
-    for (name, nargs), methods in metrics.items():
+    # Benchmark
+    for name, (nargs, methods) in metrics.items():
         print(name)
         print('-' * len(name))
 
@@ -155,42 +183,74 @@ if __name__ == '__main__':
             'time-async': []
         }
 
-        for key, method in methods.items():
-            if hasattr(method, 'to'):
-                method.to(x.device)
+        with contextlib.nullcontext() if grad else torch.no_grad():
+            for key, method in methods.items():
+                if hasattr(method, 'to'):
+                    method.to(x.device)
 
-            if key.startswith('sk.'):
-                a, b = truth, noisy
-            else:
-                a, b = x, y
+                if key.startswith('sk.'):
+                    a, b = truth, noisy
+                else:
+                    a, b = x, y
 
-            if nargs == 1:
-                f = lambda: method(a).mean()
-            else:
-                f = lambda: method(a, b).mean()
+                if nargs == 1:
+                    f = lambda: method(a).mean()
+                else:
+                    f = lambda: method(a, b).mean()
 
-            if key.endswith('-loss'):
-                key = key.replace('-loss', '')
-                g = lambda: 1. - f()
-            elif key.endswith('-halfloss'):
-                key = key.replace('-halfloss', '')
-                g = lambda: 1. - 2. * f()
-            else:
-                g = f
+                if key.endswith('-loss'):
+                    key = key.replace('-loss', '')
+                    g = lambda: 1. - f()
+                elif key.endswith('-halfloss'):
+                    key = key.replace('-halfloss', '')
+                    g = lambda: 1. - 2. * f()
+                else:
+                    g = f
 
-            data['method'].append(key)
-            data['value'].append(float(g()))
+                data['method'].append(key)
+                data['value'].append(float(g()))
 
-            if key.startswith('sk.'):
-                data['time-sync'].append(0.)
-                data['time-async'].append(0.)
-                continue
+                if key.startswith('sk.'):
+                    data['time-sync'].append(-1.)
+                    data['time-async'].append(-1.)
+                    continue
 
-            _ = timeit(g, n=42)  # activate JIT
+                _ = timeit(g, n=warm)  # activate JIT
 
-            t_sync, t_async = timeit(g)
+                t_sync, t_async = timeit(g, n=loops)
 
-            data['time-sync'].append(t_sync)
-            data['time-async'].append(t_async)
+                data['time-sync'].append(t_sync)
+                data['time-async'].append(t_async)
 
-        print(pd.DataFrame(data).sort_values(by='time-sync', ignore_index=True))
+        df = pd.DataFrame(data)
+
+        print(df.sort_values(by='time-sync', ignore_index=True))
+        print()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Benchmark')
+
+    parser.add_argument('-u', '--url', default=LENNA, help='image URL')
+    parser.add_argument('-m', '--metrics', nargs='+', default=[], choices=list(METRICS.keys()), help='metrics to benchmark')
+    parser.add_argument('-b', '--batch', type=int, default=1, help='batch size')
+    parser.add_argument('-w', '--warm', type=int, default=69, help='number of warming loops')
+    parser.add_argument('-l', '--loops', type=int, default=420, help='number of loops')
+    parser.add_argument('-d', '--device', default='cuda', choices=['cpu', 'cuda'], help='computation device')
+    parser.add_argument('-grad', default=True, action='store_false', help='enable gradients')
+    parser.add_argument('-backend', default=True, action='store_false', help='enable backends')
+
+    args = parser.parse_args()
+
+    main(
+        metrics=args.metrics,
+        url=args.url,
+        batch=args.batch,
+        warm=args.warm,
+        loops=args.loops,
+        device=args.device,
+        grad=args.grad,
+        backend=args.backend,
+    )
